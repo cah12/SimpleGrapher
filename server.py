df6ac84407bd04134d81f01786a0744cad9d4598
Corwin <corwin_hope@yahoo.com>
2025-02-05 22:36:23 -0500

    Use axios instead of AJAX

--- server.py
+++ server.py
@@ -1,13 +1,55 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify, make_response
 from waitress import serve
+
+from sympy import *
+
+def solve_for(exp, c):
+    print(exp)
+    while exp.find("^") != -1:
+        exp = exp.replace("^", "**")
+
+    # print(exp)
+    var = Symbol(c)
+    arr = exp.split('=')
+    
+    if len(arr) == 1:
+        arr.append("0")  
+    
+    result = []
+    try:
+        solutions = solve(Eq(parse_expr(arr[0]), parse_expr(arr[1])), var)        
+        for solution in solutions:
+            _str = str(solution.evalf())
+            while _str.find("**") != -1:
+                _str = _str.replace("**", "^")
+            if _str.find("I") == -1:
+                result.append(_str)
+        
+    except BaseException as error:
+        # result = 'An exception occurred: {}'.format(error)
+        result = []
+
+    return result
+
+
+
 
 app = Flask(__name__)
 @app.route("/")
 @app.route("/index")
-
 def index():
     return render_template("index.html")
-    
+
+@app.route("/csolve", methods=['POST'])
+def csolve():
+    data = request.get_json()    
+    exp = data["exp"]
+    var = data["var"]
+    # print(exp)
+    # print(var)
+    result = solve_for(exp, var) 
+    # print(result)   
+    return jsonify({"result": result})    
 
 
 if __name__ == "__main__":
---
+++ static/csolve.js
@@ -0,0 +1,26 @@
+// console.log("object");
+async function solve_for(exp, c) {
+  let res = [];
+
+  //uncomment the code below to test in the in the small UI
+  /* try {
+    const _res = await axios.post("/csolve", { exp: exp, var: c });
+    res = _res.data.result;
+    if (!res.length) {
+      $("#out").html(`Unable to find a solution`);
+    } else {
+      $("#out").html(`${c} = ${res.toString().replaceAll(",", " or ")}`);
+    }
+  } catch (error) {
+    console.log(error.toString());
+    $("#out").html(error.toString());
+  } */
+
+  try {
+    res = await axios.post("/csolve", { exp: exp, var: c });
+  } catch (error) {
+    console.error(error);
+  }
+  //console.log(res.data.result);
+  return res.data.result;
+}
