3148744d5fefea792bd210991867a9db2cb6015f
Corwin <corwin_hope@yahoo.com>
2025-02-27 22:05:56 -0500

    Revised

--- server.py
+++ server.py
@@ -151,7 +151,7 @@ def discontinuities(exp_, lower, upper, _var):
     discount = []  
     v = sp.Symbol(_var)
 
-    _exp = sp.simplify(exp_, rational=False)
+    _exp = sp.simplify(exp_, rational=True)
     # _exp=sp.parse_expr(exp_) 
     # 
     num, denom = _exp.as_numer_denom()    
