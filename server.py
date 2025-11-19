c933bb62d18a32b21a38ccad38999e89a7f89040
Corwin <corwin_hope@yahoo.com>
2025-11-19 16:42:15 -0500

    Modified analyze_discontinuity_type() to return limit value for removable discontinuities.

--- server.py
+++ server.py
@@ -414,7 +414,7 @@ def analyze_discontinuity_type(
 
         # Removable discontinuity (limits equal but function undefined)
         if left_limit == right_limit and left_limit.is_finite:
-            return 'removable'
+            return ['removable', left_limit]
 
         # Jump discontinuity
         if left_limit != right_limit and left_limit.is_finite and right_limit.is_finite:
@@ -462,7 +462,10 @@ def find_discontinuities_detailed(
     detailed_results = []
     for disc in discontinuities:
         disc_type = analyze_discontinuity_type(expr, disc, var)
-        detailed_results.append([disc, disc_type])
+        if isinstance(disc_type, list):
+            detailed_results.append([disc, disc_type[0], disc_type[1]])
+        else:
+            detailed_results.append([disc, disc_type])
 
     return detailed_results
 
