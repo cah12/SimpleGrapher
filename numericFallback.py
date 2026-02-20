bd0279faf00f78d11066ad480fde3a2f01f826eb
Corwin Hope <corwin_hope@yahoo.com>
2026-02-19 20:19:22 -0500

    Revised

--- numericFallback.py
+++ numericFallback.py
@@ -110,6 +110,15 @@ def generate_implicit_plot_points(expr, x_min=-10.0, x_max=10.0, y_min=-10.0, y_
             for segment in level_segments:
                 # segment is a NumPy array of shape (n_points, 2), where each row is [x, y]
                 # all_points.append(segment)
+
+
+                # Remove NaN values
+                valid_mask = ~np.isnan(segment).any(axis=1)
+                if not np.any(valid_mask):
+                    segment = None  # Clear reference to segment to free memory
+                    continue
+
+                segment = segment[valid_mask]
                 all_points.append(segment.tolist())
                 segment = None  # Clear reference to segment to free memory
              
