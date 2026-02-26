9a75cc152500119c2a1eb50774cae7c23eb0a30a
Corwin <corwin_hope@yahoo.com>
2026-02-26 02:08:08 -0500

    Revised

--- numericFallback.py
+++ numericFallback.py
@@ -1,21 +1,19 @@
-import gc
-from my_misc import estimate_y_bounds, sanitize_contour_segments
-from degree_radian import sin_mode, cos_mode, tan_mode, cot_mode, sec_mode, csc_mode, asin_mode, acos_mode, atan_mode, acot_mode, asec_mode, acsc_mode, trig_substitutions
-from domain_finder import closer_boundary
-
-import sympy as sp
-from sympy import symbols, solve, plot_implicit
-import numpy as np
-from scipy.optimize import fsolve, root, root_scalar
-from sympy.functions.elementary.trigonometric import TrigonometricFunction
-
-from sympy import lambdify
-
-from custom import custom
-
-import matplotlib
-matplotlib.use('Agg')
+# fmt: off
+import matplotlib 
+matplotlib.use('Agg') 
 import matplotlib.pyplot as plt
+from custom import custom
+from sympy import lambdify
+from sympy.functions.elementary.trigonometric import TrigonometricFunction
+from scipy.optimize import fsolve, root, root_scalar
+import numpy as np
+from sympy import symbols, solve, plot_implicit
+import sympy as sp
+from domain_finder import closer_boundary
+from degree_radian import sin_mode, cos_mode, tan_mode, cot_mode, sec_mode, csc_mode, asin_mode, acos_mode, atan_mode, acot_mode, asec_mode, acsc_mode, trig_substitutions
+from my_misc import estimate_y_bounds, sanitize_contour_segments
+import gc
+
 
 
 
@@ -26,7 +24,7 @@ x, y = symbols('x y')
 def estimate_y_bounds2(equation, x_min, x_max, num_x=400, y_min=None, y_max=None, y_samples=400, match_tol=None, f_tol=1e-15):
     if (equation.has(TrigonometricFunction)) or ("_mode" in str(equation)):
         return (-300, 300)
-    
+
     # Estimate y-range from symbolic solutions if possible
     x_vals = np.linspace(x_min, x_max, num_x)
     if y_min is None or y_max is None:
@@ -66,14 +64,10 @@ def estimate_y_bounds2(equation, x_min, x_max, num_x=400, y_min=None, y_max=None
     return (y_min, y_max)
 
 
-def generate_implicit_plot_points(expr, x_min=-10.0, x_max=10.0, has_discontinuity=False, y_min=-10.0, y_max=10.0):  
-    
-    
-
+def generate_implicit_plot_points(expr, x_min=-10.0, x_max=10.0, has_discontinuity=False, y_min=-10.0, y_max=10.0):
 
     (_y_min, _y_max) = estimate_y_bounds2(expr, x_min, x_max)
     # (_y_min, _y_max) = (x_min, x_max)
-
 
     y_min = min(y_min, _y_min)
     y_max = max(y_max, _y_max)
@@ -89,9 +83,9 @@ def generate_implicit_plot_points(expr, x_min=-10.0, x_max=10.0, has_discontinui
     # Convert the expression to a string
     expr_str = str(expr)
     if expr.has(TrigonometricFunction):
-        z_val = np.maximum(z_val,10)
+        z_val = np.maximum(z_val, 10)
     if ("_mode" in expr_str):
-        z_val = np.maximum(z_val,13)
+        z_val = np.maximum(z_val, 13)
     else:
         z_val = np.maximum(z_val, 15)
     # if abs(z_val) >= 1e100:
@@ -102,7 +96,8 @@ def generate_implicit_plot_points(expr, x_min=-10.0, x_max=10.0, has_discontinui
         # Replace inf with nan to avoid issues in contouring
         z[np.isinf(z)] = np.nan
         CS = plt.contour(X, Y, np.ma.masked_invalid(
-            z), levels=[0])  # Use _legacy_new_subsegments=False for use_legacy_contour=True)
+            # Use _legacy_new_subsegments=False for use_legacy_contour=True)
+            z), levels=[0])
         # CS = plt.contour(X, Y, np.ma.masked_invalid(
         #     z), levels=[0], colors='blue', alpha=0)
 
@@ -125,25 +120,24 @@ def generate_implicit_plot_points(expr, x_min=-10.0, x_max=10.0, has_discontinui
                     # continue
                     pass
 
-
                 all_points.append(segment.astype(np.float32))
                 # segment = None  # Clear reference to segment to free memory
-        # all_segments = None  # Clear reference to all_segments to free memory    
+        # all_segments = None  # Clear reference to all_segments to free memory
         # all_points = np.array(all_points)
-        all_points = sanitize_contour_segments(expr, all_points, x_min, x_max, has_discontinuity)
+        all_points = sanitize_contour_segments(
+            expr, all_points, x_min, x_max, has_discontinuity)
         # del CS
         plt.clf()
         plt.cla()
         # gc.collect()
         # plt.close()
         # Mandatory cleanup
-        
+
         plt.close('all')
         del CS
-        gc.collect() # Force garbage collection
+        gc.collect()  # Force garbage collection
         return all_points
 
     except Exception as e:
         print(f"Error generating implicit plot points: {e}")
         return []
-
