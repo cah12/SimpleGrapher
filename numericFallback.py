d8ff0f796a4797220f25dd955348c1cf4ab9f172
Corwin <corwin_hope@yahoo.com>
2026-02-26 19:02:51 -0500

    Revised

--- numericFallback.py
+++ numericFallback.py
@@ -72,9 +72,9 @@ def generate_implicit_plot_points(expr, x_min=-10.0, x_max=10.0, has_discontinui
     y_min = min(y_min, _y_min)
     y_max = max(y_max, _y_max)
 
-    num_points = 4000
-    _x = np.linspace(x_min, x_max, np.round(num_points/2).astype(int))
-    _y = np.linspace(y_min, y_max, num_points)
+
+    _x = np.linspace(x_min, x_max,450)
+    _y = np.linspace(y_min, y_max, 900)
 
     X, Y = np.meshgrid(_x, _y)
     # z = x**2 + y**2 - 1  # Example: circle equation x^2 + y^2 = 1
@@ -91,7 +91,7 @@ def generate_implicit_plot_points(expr, x_min=-10.0, x_max=10.0, has_discontinui
         z_val = np.maximum(z_val, 15)
     # if abs(z_val) >= 1e100:
     # z_val = 10
-    z_val = np.percentile(z[~np.isnan(z)], 99)
+    # z_val = np.percentile(z[~np.isnan(z)], 99)
     # z_val = np.nanmean(z)+3*np.nanstd(z)
     # z_val = np.nanmax(z)*0.90
     # print(z_val)
