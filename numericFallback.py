de217802c4fce1ed0091f4ff49061a488058c6de
Corwin <corwin_hope@yahoo.com>
2026-02-28 22:13:32 -0500

    Revised

--- numericFallback.py
+++ numericFallback.py
@@ -75,7 +75,7 @@ def generate_implicit_plot_points(expr, x_min=-10.0, x_max=10.0, has_discontinui
     y_min = min(y_min, _y_min)
     y_max = max(y_max, _y_max)
 
-    num_points = 1700
+    num_points = 1200
     _x = np.linspace(x_min, x_max, num_points)
     _y = np.linspace(y_min, y_max, num_points)
 
